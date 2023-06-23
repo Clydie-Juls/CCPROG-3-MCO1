5426cb8e76477927a3f98c8e87b49bc0ec8ae84a
UnityGrave <72306255+UnityGrave@users.noreply.github.com>
2023-06-23 16:29:22 +0800

    Write javadoc for VendingMachine

--- Project/src/model/VendingMachine.java
+++ Project/src/model/VendingMachine.java
@@ -2,19 +2,46 @@ package model;
 
 import java.util.Map;
 
+/**
+ * The VendingMachine class represents a vending machine that contains slots with items.
+ */
 public class VendingMachine {
     private final Slot[] slots;
     private final Denomination denomination;
     private final Transactions transactions;
 
+    /**
+     * Constructs a VendingMachine object with default settings.
+     */
     public VendingMachine() {
         this(8, 10);
     }
 
+    /**
+     * Constructs a VendingMachine object with the specified number of slots and item capacity.
+     *
+     * @param noOfSlots    the number of slots in the vending machine
+     * @param itemCapacity the maximum capacity of each slot for holding items
+     */
+    public VendingMachine(int noOfSlots, int itemCapacity) {
+        denomination = new Denomination();
+        transactions = new Transactions();
+        slots = new Slot[noOfSlots];
+        for (int i = 0; i < slots.length; i++) {
+            slots[i] = new Slot(itemCapacity);
+        }
+    }
+
+    /**
+     * Constructs a VendingMachine object with the specified number of slots or capacity.
+     *
+     * @param no               the number of slots or capacity based on the value of isNoOfCapacity
+     * @param isNoOfCapacity   determines whether the value of no represents the number of slots or the item capacity
+     */
     public VendingMachine(int no, boolean isNoOfCapacity) {
         denomination = new Denomination();
         transactions = new Transactions();
-        if(isNoOfCapacity) {
+        if (isNoOfCapacity) {
             no = Math.max(8, no);
             slots = new Slot[no];
             for (int i = 0; i < slots.length; i++) {
@@ -29,25 +56,30 @@ public class VendingMachine {
         }
     }
 
-    public VendingMachine(int noOfSlots, int itemCapacity) {
-        denomination = new Denomination();
-        transactions = new Transactions();
-        slots = new Slot[noOfSlots];
-        for (int i = 0; i < slots.length; i++) {
-            slots[i] = new Slot(itemCapacity);
-        }
-    }
-
+    /**
+     * Retrieves the array of slots in the vending machine.
+     *
+     * @return the array of slots
+     */
     public Slot[] getSlots() {
         return slots;
     }
 
+    /**
+     * Retrieves the denomination object representing the currency used in the vending machine.
+     *
+     * @return the denomination object
+     */
     public Denomination getDenomination() {
         return denomination;
     }
 
+    /**
+     * Retrieves the transactions object representing the logs of item transactions.
+     *
+     * @return the transactions object
+     */
     public Transactions getTransactions() {
         return transactions;
     }
-
 }
