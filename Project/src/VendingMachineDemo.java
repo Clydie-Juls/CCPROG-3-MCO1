0c8abbfca63242266ce14c072b28f90563cb9924
UnityGrave <72306255+UnityGrave@users.noreply.github.com>
2023-06-26 16:48:55 +0800

    Write JavaDoc & Adjust prompt messages in VendingMachineDemo

--- Project/src/VendingMachineDemo.java
+++ Project/src/VendingMachineDemo.java
@@ -6,10 +6,14 @@ import state.Command;
 import java.util.Map;
 import java.util.Scanner;
 
+/**
+ * The VendingMachineDemo class represents a demo program for a vending machine.
+ */
 public class VendingMachineDemo {
     private static Scanner scanner;
     private static VendingMachineController vendingMachineController;
     private static MaintenanceService maintenanceService;
+
     public static void main(String[] args) {
         vendingMachineController = new VendingMachineController(new VendingMachine());
         maintenanceService = new MaintenanceService(vendingMachineController.getvendingMachine());
@@ -21,11 +25,15 @@ public class VendingMachineDemo {
         scanner.close();
     }
 
+    /**
+     * Executes the main options/features of the vending machine demo program.
+     */
     private static void executeOptionFeatures() {
         Command[] commands = {
                 Command.VENDING_MACHINE_FEATURES,
                 Command.MAINTENANCE_FEATURES,
-                Command.EXIT};
+                Command.EXIT
+        };
 
         Command currCommand;
 
@@ -40,11 +48,11 @@ public class VendingMachineDemo {
             }
 
         } while (currCommand != Command.EXIT);
-
-
-
     }
 
+    /**
+     * Executes the vending machine features/options.
+     */
     private static void executeVendingMachineFeatures() {
         Command[] commands = {
                 Command.BUY,
@@ -59,9 +67,8 @@ public class VendingMachineDemo {
             displayCommands(commands);
             currCommand = inputCommand(commands);
             switch (currCommand) {
-                // buy method will be modified.
                 case BUY -> {
-                    if(!vendingMachineController.buy()) {
+                    if (!vendingMachineController.buy()) {
                         System.out.println("Buy process Unsuccessful!!!");
                     }
                 }
@@ -72,6 +79,9 @@ public class VendingMachineDemo {
         } while (currCommand != Command.EXIT);
     }
 
+    /**
+     * Executes the maintenance features/options.
+     */
     private static void executeMaintenanceFeatures() {
         Command[] commands = {
                 Command.STOCK,
@@ -95,17 +105,17 @@ public class VendingMachineDemo {
                     String itemName = scanner.nextLine();
                     System.out.println("Input amount:");
                     int amount = scanner.nextInt();
-                    System.out.println("How much of each item:");
+                    System.out.println("How much does each item cost?");
                     int price = scanner.nextInt();
                     System.out.println("Input calories:");
                     int calories = scanner.nextInt();
-                    System.out.println("Which slot number should the item be putted?");
+                    System.out.println("Which slot number should the item be placed in?");
                     int slotNo = scanner.nextInt();
                     maintenanceService.stock(amount, slotNo, itemName, calories, price);
                 }
 
                 case RESTOCK -> {
-                    System.out.println("Which slot number should it refill the item?");
+                    System.out.println("Which slot number should be refilled?");
                     int slotNo = scanner.nextInt();
                     System.out.println("Input amount:");
                     int amount = scanner.nextInt();
@@ -115,15 +125,15 @@ public class VendingMachineDemo {
                 case COLLECT_MONEY -> maintenanceService.collectMoney();
 
                 case REPLENISH_MONEY -> {
-                    System.out.println("How many should I refill each denomination?");
+                    System.out.println("How many of each denomination should be refilled?");
                     int amount = scanner.nextInt();
                     maintenanceService.replenishDenomination(amount);
                 }
 
                 case CHANGE_ITEM_PRICE -> {
-                    System.out.println("Which slot number should it change the item's price?");
+                    System.out.println("Which slot number should the item's price be changed?");
                     int slotNo = scanner.nextInt();
-                    System.out.println("How much?");
+                    System.out.println("Enter the new price:");
                     int price = scanner.nextInt();
                     maintenanceService.changeItemPrice(slotNo, price);
                 }
@@ -135,34 +145,43 @@ public class VendingMachineDemo {
         } while (currCommand != Command.EXIT);
     }
 
+    /**
+     * Reads and validates the user's command input.
+     *
+     * @param availableCommands the array of available commands
+     * @return the command entered by the user
+     */
     private static Command inputCommand(Command[] availableCommands) {
         Command command;
         do {
             String input = scanner.nextLine();
             command = Command.inputCommand(input);
-            if(command == null) {
+            if (command == null) {
                 Integer commandIndex;
                 try {
                     commandIndex = Integer.parseInt(input) - 1;
                 } catch (NumberFormatException e) {
                     commandIndex = null;
                 }
-                if(commandIndex >= 0 && commandIndex < availableCommands.length) {
+                if (commandIndex != null && commandIndex >= 0 && commandIndex < availableCommands.length) {
                     command = availableCommands[commandIndex];
                 } else {
-                    System.out.println("Command Index Out of bounds.");
+                    System.out.println("Command Index Out of Bounds.");
                 }
-
             }
         } while (command == null);
 
         return command;
     }
 
+    /**
+     * Displays the available commands/options to the user.
+     *
+     * @param commands the array of commands/options to display
+     */
     private static void displayCommands(Command[] commands) {
         for (int i = 0; i < commands.length; i++) {
-            System.out.printf("[%d] - ", i + 1);
-            System.out.println(commands[i].toString().toLowerCase());
+            System.out.printf("[%d] - %s%n", i + 1, commands[i].toString().toLowerCase());
         }
     }
 }
